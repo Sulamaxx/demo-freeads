98fdbb4164db549e675afcf0c07e4b1b9922a3ac
Sulamaxx <115876513+Sulamaxx@users.noreply.github.com>
2023-07-17 19:24:58 +0530

    Initial commit

---
+++ src/test/java/com/codevalor/demo/freeads/DemoFreeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codevalor.demo.freeads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFreeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
