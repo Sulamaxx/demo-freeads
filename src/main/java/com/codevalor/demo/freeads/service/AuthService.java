35f5f95035a48511a4bbb7ed7b700c63a8d2cb27
Poojitha Irosha <poojithairosha9311@gmail.com>
2023-07-19 00:55:20 +0530

    Updated authentication flow

--- src/main/java/com/codevalor/demo/freeads/service/AuthService.java
+++ src/main/java/com/codevalor/demo/freeads/service/AuthService.java
@@ -2,6 +2,7 @@ package com.codevalor.demo.freeads.service;
 
 import com.codevalor.demo.freeads.dto.request.LoginRequest;
 import com.codevalor.demo.freeads.dto.response.LoginResponse;
+import com.codevalor.demo.freeads.model.User;
 import com.codevalor.demo.freeads.repository.UserRepository;
 import com.codevalor.demo.freeads.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
@@ -26,11 +27,16 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     public LoginResponse login(LoginRequest loginRequest){
+        User user = userRepository.findByEmail(loginRequest.email()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if(user.getExternalId() != null) {
+            throw new RuntimeException("User is registered with social login, please login with social login");
+        }
+
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        UserDetails userDetails = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        String jwtToken = jwtUtil.generateToken(userDetails, Map.of("role", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining())));
+        String jwtToken = jwtUtil.generateToken(user, Map.of("role", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining())));
         return LoginResponse.builder().token(jwtToken).build();
     }
 
